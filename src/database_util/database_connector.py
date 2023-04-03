9073369af9b7b6493a3c9f52f14d9d1b56e6aef2
Sorravit Bunjongpean <sorravit@Sorravits-Tiny-MacBook-Pro.local>
2023-04-03 15:46:34 +0700

    get table columns insert mock data to table without dependency

--- src/database_util/database_connector.py
+++ src/database_util/database_connector.py
@@ -1,3 +1,7 @@
+import string
+import random
+from datetime import datetime
+
 import psycopg2
 
 
@@ -47,3 +51,81 @@ class DatabaseConnector:
         cur.close()
         self.conn.close()
         return table_relationships
+
+    def get_table_columns(self, table_name):
+        """
+        Queries the name and data type of columns within a given table.
+
+        Args:
+            table_name (str): The name of the table to query.
+            conn (psycopg2.extensions.connection): The database connection object.
+
+        Returns:
+            list: A list of dictionaries representing the columns in the table, where each dictionary has keys 'name'
+            and 'type'.
+            :param table_name:
+            :param self:
+        """
+
+        self.connect_to_database()
+        cur = self.conn.cursor()
+
+        cur.execute(f"SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{table_name}'")
+
+        columns = []
+        for column in cur.fetchall():
+            columns.append({'name': column[0], 'type': column[1]})
+
+        cur.close()
+
+        return columns
+
+    def insert_mock_data(self, table_name, columns):
+        """
+        Insert mock data into the specified table.
+
+        Args:
+            table_name (str): The name of the table to insert the data into.
+            columns (list[dict]): A list of dictionaries representing the columns in the table, where each dictionary has
+            keys 'name' and 'type'.
+        """
+        # Open a cursor to perform database operations
+        self.connect_to_database()
+        conn = self.conn
+        cur = self.conn.cursor()
+
+        # Generate the insert statement
+        column_names = [column['name'] for column in columns if column['name'] != 'id']
+        placeholders = ','.join(['%s'] * len(column_names))
+        sequence_name = f"{table_name}_seq"
+        insert_statement = f"INSERT INTO {table_name} (id,{','.join(column_names)}) VALUES (nextval('{sequence_name}'),{placeholders})"
+
+        # Generate mock data
+        num_records = 10
+        mock_data = []
+        for i in range(num_records):
+            record = []
+            for column in columns:
+                if column['name'] == 'id':
+                    continue
+                elif column['type'] == 'text':
+                    record.append(''.join(random.choices(string.ascii_uppercase + string.digits, k=10)))
+                elif column['type'] == 'character varying':
+                    record.append(''.join(random.choices(string.ascii_uppercase + string.digits, k=10)))
+                elif column['type'] == 'integer':
+                    record.append(random.randint(1, 100))
+                elif column['type'] == 'boolean':
+                    record.append(random.choice([True, False]))
+                elif column['type'] == 'bytea':
+                    record.append(bytes([random.randint(0, 255) for _ in range(10)]))
+                elif column['type'] == 'timestamp without time zone':
+                    record.append(datetime.now())
+            mock_data.append(record)
+
+        # Insert the mock data
+        print(insert_statement)
+        print(mock_data)
+        cur.executemany(insert_statement, mock_data)
+        conn.commit()
+
+        cur.close()
