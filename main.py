e0b84f790d886a0c75f6cab0b1b47459d7906112
Big <sorravit16758@gmail.com>
2024-12-09 23:00:56 +0700

    support mysql, experimental

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # This is a sample Python script.
-from src.database_util.database_connector import DatabaseConnector
+# from src.database_util.database_connector import DatabaseConnector
+from src.database_util.database_util import print_database_relations
+from src.database_util.mysql_connector import MySqlConnector
 from src.database_util.relationship_sorter import get_table_populate_order
 
 
@@ -15,7 +17,10 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    database_connector = DatabaseConnector(host="localhost", database="postgres", user="sorravit", password="sorravit")
+    # database_connector = DatabaseConnector(host="localhost", database="etaxportal", user="root", password="sorravit")
+    database_connector = MySqlConnector(host="localhost", database="etaxportal", user="root", password="sorravit",
+                                        schema='etaxportal')
+    # database_connector = MySqlConnector(host="localhost", database="sorravit", user="root", password="sorravit",schema='sorravit')
     table_relation = database_connector.get_table_relationships()
 
     # Preserve the legacy format
@@ -24,11 +29,15 @@ if __name__ == '__main__':
     # print_database_relations(key_dependency_pairs)
     table_order_list = get_table_populate_order(key_dependency_pairs)
 
-    # print(table_order_list[7])
-    # print(table_relation[table_order_list[7]])
+    # tableOrder = 0
+    # print(table_order_list[tableOrder])
+    # print(table_relation[table_order_list[tableOrder]])
+    # print("get_primary_key: " + database_connector.get_primary_key(table_order_list[tableOrder]))
+    # print(table_order_list)
     for table_name in table_order_list:
         if table_name != 'flyway_schema_history':
             print("Inserting data to :" + table_name)
+            print("get_primary_key: " + database_connector.get_primary_key(table_name))
             columns = database_connector.get_table_columns(table_name)
             print("columns " + str(columns))
             database_connector.insert_mock_data(table_name, columns, table_relation[table_name])
--- src/database_util/mysql_connector.py
+++ src/database_util/mysql_connector.py
@@ -1,24 +1,25 @@
+import json
 import string
 import random
 from datetime import datetime
+import mysql.connector
 
-import psycopg2
 
-
-class DatabaseConnector:
-    def __init__(self, host: str, database: str, user: str, password: str):
+class MySqlConnector:
+    def __init__(self, host: str, database: str, user: str, password: str, schema: str):
         self.conn = None
         self.host = host
         self.database = database
         self.user = user
         self.password = password
+        self.schema = schema
 
     def connect_to_database(self):
-        self.conn = psycopg2.connect(
+        self.conn = mysql.connector.connect(
             host=self.host,
             database=self.database,
             user=self.user,
-            password=self.password
+            password=self.password,
         )
 
     def get_table_relationships(self):
@@ -27,25 +28,25 @@ class DatabaseConnector:
         cur.execute("""
             SELECT table_name
             FROM information_schema.tables
-            WHERE table_schema = 'public'
-        """)
+            WHERE table_schema = %s
+        """, (self.schema,))
         tables = cur.fetchall()
+
         table_relationships = {}
         for table in tables:
-            cur.execute(f"""
-                SELECT tc.table_name, kcu.column_name,
-                ccu.table_name AS foreign_table_name,
-                ccu.column_name AS foreign_column_name
+            cur.execute("""
+                SELECT
+                    kcu.table_name,
+                    kcu.column_name,
+                    kcu.referenced_table_name AS foreign_table_name,
+                    kcu.referenced_column_name AS foreign_column_name
                 FROM
-                    information_schema.table_constraints AS tc
-                    JOIN information_schema.key_column_usage AS kcu
-                      ON tc.constraint_name = kcu.constraint_name
-                      AND tc.table_schema = kcu.table_schema
-                    JOIN information_schema.constraint_column_usage AS ccu
-                      ON ccu.constraint_name = tc.constraint_name
-                      AND ccu.table_schema = tc.table_schema
-                WHERE tc.constraint_type = 'FOREIGN KEY' AND tc.table_name='{table[0]}';
-            """)
+                    information_schema.key_column_usage AS kcu
+                WHERE
+                    kcu.referenced_table_name IS NOT NULL
+                    AND kcu.table_schema = %s
+                    AND kcu.table_name = %s
+            """, (self.schema, table[0],))
             relationships = cur.fetchall()
             table_relationships[table[0]] = {
                 'dependencies': [relationship[2] for relationship in relationships],
@@ -105,30 +106,42 @@ class DatabaseConnector:
         # Generate the insert statement
         column_names = [column['name'] for column in columns if column['name'] != 'id']
         placeholders = ','.join(['%s'] * len(column_names))
-        sequence_name = f"{table_name}_seq"
-        insert_statement = f"INSERT INTO {table_name} (id,{','.join(column_names)}) VALUES (nextval('{sequence_name}'),{placeholders})"
+        insert_statement = f"INSERT INTO `{self.schema}`.`{table_name}` (`{'`,`'.join(column_names)}`) VALUES ({placeholders})"
 
         # Generate mock data
-        num_records = 10
+        num_records = 1
         mock_data = []
         for i in range(num_records):
             record = []
+            # Need to refactor this
             for column in columns:
                 if column['name'] == 'id':
                     continue
                 elif column['name'] in dependency['dependency_columns']:
                     dependency_table_name = dependency['dependencies'][
                         dependency['dependency_columns'].index(column['name'])]
-                    cur.execute(f"SELECT id FROM {dependency_table_name}")
+                    primary_key_column_name = self.get_primary_key(dependency_table_name)
+                    cur.execute(f"SELECT `{primary_key_column_name}` FROM `{dependency_table_name}`")
                     dependency_rows = cur.fetchall()
                     dependency_ids = [row[0] for row in dependency_rows]
                     record.append(random.choice(dependency_ids))
+                elif column['type'] == 'json':
+                    json_object = {f'key{n}': f'value{n}' for n in range(random.randint(1, 10))}
+                    record.append(json.dumps(json_object))
                 elif column['type'] == 'text':
                     record.append(''.join(random.choices(string.ascii_uppercase + string.digits, k=5)))
                 elif column['type'] == 'character varying':
                     length = random.randint(1, column['max_length'])
                     record.append(''.join(random.choices(string.ascii_uppercase + string.digits, k=length)))
+                elif column['type'] == 'varchar':
+                    length = random.randint(1, column['max_length'])
+                    record.append(''.join(random.choices(string.ascii_uppercase + string.digits, k=length)))
+                elif column['type'] == 'char':
+                    length = random.randint(1, column['max_length'])
+                    record.append(''.join(random.choices(string.ascii_uppercase + string.digits, k=length)))
                 elif column['type'] == 'integer':
+                    record.append(random.randint(1, 100))
+                elif column['type'] == 'decimal':
                     record.append(random.randint(1, 100))
                 elif column['type'] == 'double precision':
                     record.append(random.uniform(1, 100))
@@ -138,12 +151,42 @@ class DatabaseConnector:
                     record.append(bytes([random.randint(0, 255) for _ in range(10)]))
                 elif column['type'] == 'timestamp without time zone':
                     record.append(datetime.now())
+                elif column['type'] == 'int':
+                    record.append(random.randint(1, 100))
+                elif column['type'] == 'double':
+                    record.append(random.uniform(1, 100))
+                elif column['type'] == 'tinyint':
+                    record.append(random.choice([1, 0]))  # Booleans in MySQL are represented as tinyint
+                elif column['type'] == 'blob':
+                    record.append(bytes([random.randint(0, 255) for _ in range(10)]))
+                elif column['type'] == 'datetime':
+                    record.append(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                else:
+                    print("Unsupported type " + column['type'])
             mock_data.append(record)
 
         # Insert the mock data
-        print(insert_statement)
+        print("insert_statement" + insert_statement)
+        print("mock data  " + str(len(mock_data[0])))
         print(mock_data)
         cur.executemany(insert_statement, mock_data)
         conn.commit()
 
         cur.close()
+
+    def get_primary_key(self, table_name):
+        # establish the database connection
+        self.connect_to_database()
+        conn = self.conn
+        cur = self.conn.cursor()
+
+        # Execute query to get the primary key of the table
+        cur.execute(f"SHOW KEYS FROM `{table_name}` WHERE Key_name = 'PRIMARY'")
+        result = cur.fetchone()
+        conn.close()
+
+        if result:
+            # position 4 seem to be column_name of the primary key
+            return result[4]
+        else:
+            return None
